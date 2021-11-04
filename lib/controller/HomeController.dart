98dcd7b47a7d394a3bae09ec6cd7d2d5ac3eaf29
Muhammad Sharjeel Awan <sharjeelmalik628@gmail.com>
2021-11-04 16:43:55 +0500

    home Screen

--- lib/controller/HomeController.dart
+++ lib/controller/HomeController.dart
@@ -1,26 +1,29 @@
 
 import 'dart:async';
+import 'package:saraa_kuch/models/Banner.dart';
+
 import '../services/HomeService.dart';
+import '../models/Product.dart';
 
 class HomeController{
 
    //Banner silder controller
-   StreamController<List<String>> _bannerController = new StreamController<List<String>>();
+   StreamController<List<Banner>> _bannerController = new StreamController<List<Banner>>();
    StreamSink _bannerSink;
    Stream bannerStream;
 
    //Specialforyou controller
-   StreamController<List<String>> _specialController = new StreamController<List<String>>();
+   StreamController<List<Banner>> _specialController = new StreamController<List<Banner>>();
    StreamSink _specialSink;
    Stream specialStream;
 
    //popular controller
-   StreamController<List<String>> _popularController = new StreamController<List<String>>();
+   StreamController<List<Product>> _popularController = new StreamController<List<Product>>();
    StreamSink _popularSink;
    Stream popularStream;
 
    //allproducts controller
-   StreamController<List<String>> _allProductController = new StreamController<List<String>>();
+   StreamController<List<Product>> _allProductController = new StreamController<List<Product>>();
    StreamSink _allProductSink;
    Stream allProductStream;
 
@@ -56,10 +59,10 @@ class HomeController{
 
     void getProductsFromApi() async{
       HomeService _homeService = new HomeService();
-      List<String> bannerList = await _homeService.getBanners();
-      List<String> categoryList = await _homeService.getCategories();
-      List<String> popularProductsList = await _homeService.getPopularProducts();
-      List<String> allProductsList = await _homeService.getAllProducts();
+      List<Banner> bannerList = await _homeService.getBanners();
+      List<Banner> categoryList = await _homeService.getCategories();
+      List<Product> popularProductsList = await _homeService.getPopularProducts();
+      List<Product> allProductsList = await _homeService.getAllProducts();
       _bannerSink.add(bannerList);
       _specialSink.add(categoryList);
       _popularSink.add(popularProductsList);
--- lib/services/HomeService.dart
+++ lib/services/HomeService.dart
@@ -1,42 +1,73 @@
+import 'dart:convert';
+
+import 'package:saraa_kuch/models/Banner.dart';
+
+import '../models/Product.dart';
+import 'package:http/http.dart' as http ;
 
 class HomeService{
 
+  String _domain = "https://saraakuch.pk/wp-json/wc/v3/";
+  String _ck = "ck_21ea4617b9a14b863492c12025502accbb383d1b";
+  String _cS = "cs_7c5b61db6b8270d01fc223c886fb11af7fd8ee0a";
+
   List<String> productsName = ["Cap","tea","shirt","fruit","shoes"];
 
-  Future<List<String>> getBanners() async{
+  Future<List<Banner>> getBanners() async{
 
-    await Future.delayed(const Duration(seconds: 5),(){
-      print("test5");
+    String url = "https://saraakuch.pk/wp-json/wp/v2/slider-images?slider_id=247641";
 
-    });
-    return  this.productsName;
-   // return [];
+    final response = await http.get(Uri.parse(url));
+
+    if(response.statusCode == 200){
+
+      return  Banner.jsonToModel(jsonDecode(response.body));
+    }
+
+    return  [];
   }
 
-  Future<List<String>> getCategories() async {
+  Future<List<Banner>> getCategories() async {
 
-    await Future.delayed(const Duration(seconds: 5),(){
-      print("test5");
+    String url = "${_domain}products/categories?consumer_key=$_ck&consumer_secret=$_cS";
 
-    });
-    return  this.productsName;
+    final response = await http.get(Uri.parse(url));
+
+    if(response.statusCode == 200){
+
+     return  Banner.jsonToModel(jsonDecode(response.body));
+    }
+
+    return  [];
   }
 
-  Future<List<String>> getPopularProducts() async {
-
-    await Future.delayed(const Duration(seconds: 5),(){
-      print("test5");
-
-    });
-    return  this.productsName;
+  Future<List<Product>> getPopularProducts() async {
+    String url = "${_domain}products?featured=true&consumer_key=$_ck&consumer_secret=$_cS";
+    //call http and get response in json
+    final response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      //convert json response to list or map
+      return Product.jsonToModel(jsonDecode(response.body));
+    }else {
+      return [];
+    }
   }
 
-  Future<List<String>> getAllProducts() async {
+  Future<List<Product>> getAllProducts() async {
 
-    await Future.delayed(const Duration(seconds: 5),(){
-      print("test5");
-
-    });
-    return  this.productsName;
+    String url = "${_domain}products?page=1&consumer_key=$_ck&consumer_secret=$_cS";
+    //call http and get response in json
+    final response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      //convert json response to list or map
+     return Product.jsonToModel(jsonDecode(response.body));
+    }else {
+      return [];
+    }
+    // await Future.delayed(const Duration(seconds: 5),(){
+    //   print("test5");
+    //
+    // });
+    //return  this.productsName;
   }
 }
